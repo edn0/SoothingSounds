1952e2fae31036b8c1c86108b743fd1743bb2157
edn0 <corentin.edno.blondiau@gmail.com>
2022-09-15 18:15:05 +0200

    added icons and some sfx

--- script.js
+++ script.js
@@ -14,18 +14,32 @@ function get_sliders_value () {
     let sliders_value_string = perc_slider1 + " S1 ; " + perc_slider2 + " S2 ; " + perc_slider3 + " S3 ;"
 
 
-    let volume = sliders_values[0];
-    console.log(volume + "volume");
-    rain_fx.volume = volume/100;
+    let volume0 = sliders_values[0];
+    console.log(volume0 + "volume");
+    rain_fx.volume = volume0/100;
     rain_fx.muted = false;
+
+    let volume2 = sliders_values[2];
+    group_fx.volume = volume2/100;
+    group_fx.muted = false;
 
 }
 
+
+//TODO 
+// Make a cut all sound btn
+// Make functions for audio
 
 let rain_fx = new Audio("sounds/sfx/raindrops_soe.wav")
 rain_fx.muted = true;
 rain_fx.play();
 rain_fx.loop = true;
+
+
+let group_fx = new Audio("sounds/sfx/crowd_talking_fx.wav")
+group_fx.muted = true;
+group_fx.play();
+group_fx.loop = true;
 
 window.onload = setInterval(get_sliders_value, 2);
 
