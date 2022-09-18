dd7ee7ab3b498c1411b8f7a8c6b6a603965dcc63
edno <corentin.edno.blondiau@gmail.com>
2022-09-18 11:21:41 +0200

    added music, can't toggle instruments on or off

--- script.js
+++ script.js
@@ -1,61 +1,116 @@
 let sliders_values = [];
 
-let sfx = [];
+let sounds = [];
+let music = [];
 
 let thunder_sfx = [];
+
+
 
 function get_sliders_value () {
 
     sliders_values = [];
 
 
-    let perc_slider1 = document.getElementById("slider1").value;
-    let perc_slider2 = document.getElementById("slider2").value;
-    let perc_slider3 = document.getElementById("slider3").value;
+    let slider1 = document.getElementById("slider1").value;
+    let slider2 = document.getElementById("slider2").value;
+    let slider3 = document.getElementById("slider3").value;
 
-    sliders_values.push(perc_slider1, perc_slider2, perc_slider3);
-
-    let sliders_value_string = perc_slider1 + " S1 ; " + perc_slider2 + " S2 ; " + perc_slider3 + " S3 ;"
-
+    sliders_values.push(slider1, slider2, slider3);
 
     let volume0 = sliders_values[0];
     console.log(volume0 + "volume");
-    rain_fx.volume = volume0/100;
-    rain_fx.muted = false;
+    sounds[0].volume = volume0/100;
+    sounds[0].muted = false;
 
     let volume1 = sliders_values[1];
-    thunder_fx.volume = volume1/100;
-    thunder_fx.muted = false;
+    sounds[1].volume = volume1/100;
+    sounds[1].muted = false;
 
     let volume2 = sliders_values[2];
-    group_fx.volume = volume2/100;
-    group_fx.muted = false;
+    sounds[2].volume = volume2/100;
+    sounds[2].muted = false;
+
+    let volume3 = document.getElementById("music_volume_slider").value;
+
+    for (i in music) {
+        music[i].volume = volume3/100;
+        music[i].muted = false;
+    }
 
 }
 
 
+function loadSound (sound_name) {
+    s = new Audio("./sounds/sfx/" + sound_name + ".wav");
+    s.muted = true;
+    s.play();
+    s.loop = true;
+
+    sounds.push(s);
+}
+
+
+function loadMusic (music_name) {
+    let m = new Audio("./sounds/music/" + music_name + ".mp3");
+    m.muted = true;
+    m.play();
+    m.loop = true;
+
+    music.push(m)
+
+}
+
+
+
 //TODO 
-// Make a cut all sound btn
-// Make functions for audio
+// Make a
 
-let rain_fx = new Audio("sounds/sfx/raindrops_soe.wav")
-rain_fx.muted = true;
-rain_fx.play();
-rain_fx.loop = true;
+function startUp () {
+    loadSound("rain");
+    loadSound("thunder");
+    loadSound("talking");
+
+    loadMusic("keys");
+    loadMusic("drums");
+    loadMusic("sax");
+
+    for (i in sounds) {
+        sounds[i].volume = 0;
+        sounds[i].muted = true;
+    }
+
+    for (i in music) {
+        music[i].volume = 0;
+        sounds[i].muted = true;
+    }
+
+    document.getElementById("slider1").value = 0;
+    document.getElementById("slider2").value = 0;
+    document.getElementById("slider3").value = 0;
 
 
-let group_fx = new Audio("sounds/sfx/crowd_talking_fx.wav")
-group_fx.muted = true;
-group_fx.play();
-group_fx.loop = true;
+    document.getElementById("music_volume_slider").value = 0;
+    
+}
 
-let thunder_fx = new Audio("sounds/sfx/distant-thunder-01.wav")
-thunder_fx.muted = true;
-thunder_fx.play();
-thunder_fx.loop = true;
-
-window.onload = setInterval(get_sliders_value, 2);
+function toggle(n) {
 
 
-console.table(sliders_values);
+    if (music[n].muted == true) {
+        music[n].muted = false;
+    }
+
+    if (music[n].muted == false) {
+        music[n].muted = true;
+    }
+}
+
+window.onload = startUp();
+
+setInterval(get_sliders_value, 100);
+
+
+console.table(music);
+
 
